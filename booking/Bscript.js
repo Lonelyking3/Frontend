2a6c9c4487f4d8c35667ded3e4b829e88ec500bd
LK10 <90304508+Lonelyking3@users.noreply.github.com>
2024-09-06 20:59:20 +0530

    Booked page

--- booking/Bscript.js
+++ booking/Bscript.js
@@ -1,3 +1,44 @@
+// Dropdown content
+
+function select(){
+    Adult = document.getElementById('adult').value;
+    Child = document.getElementById('child').value;
+    Infant = document.getElementById('infant').value;
+
+    let passangerText = `${Adult} Adult`;
+    if(Adult > 1){
+        passangerText = `${Adult} Adults`;
+    }
+
+    if(Child > 0){
+        if(Child == 1){
+            passangerText += `, ${Child} Child`;
+        }
+        else{
+            passangerText += `, ${Child} Children`;
+        }
+    }
+    
+    if(Infant > 0){
+        if(Infant == 1){
+            passangerText += `, ${Infant} Infant`;
+        }
+        else{
+            passangerText += `, ${Infant} Infants`;
+        }
+    }
+
+
+    document.querySelector(".passangers").value = passangerText;
+    
+}
+
+document.addEventListener("DOMContentLoaded",function(){
+    select();
+});
+
+//Main Content
+
 document.addEventListener("DOMContentLoaded", function() {
     loadBooking();
 });
@@ -7,32 +48,35 @@ function loadBooking() {
 
     if (bookings.length > 0) {
         const latestBooking = bookings[bookings.length - 1];
-        document.getElementById("name").value = latestBooking.Name || '';
         document.getElementById("dep").value = latestBooking.departureCity || '';
         document.getElementById("arr").value = latestBooking.arrivalCity || '';
         document.getElementById("depD").value = latestBooking.departureDate || '';
         document.getElementById("arrD").value = latestBooking.arrivalDate || '';
 
         document.getElementById('no2').innerHTML =
-         `<h4 class="Dep">${latestBooking.departureCity}</h4> 
+        `<h4 class="Dep">${latestBooking.departureCity}</h4> 
+        <span class="aro"><i class="fa fa-arrows-h"></i></span>
         <h4 class="Ret">${latestBooking.arrivalCity}</h4> 
         <h4 class="Frm">${latestBooking.departureDate}</h4> 
         <h4 class="Reto">${latestBooking.arrivalDate}</h4> 
         <button onclick="Book()" class="Bookb">Book</button>`;
+       
         
         document.getElementById('no3').innerHTML =
-         `<h4 class="Dep">${latestBooking.departureCity}</h4> 
+        `<h4 class="Dep">${latestBooking.departureCity}</h4> 
+        <span class="aro"><i class="fa fa-arrows-h"></i></span>
         <h4 class="Ret">${latestBooking.arrivalCity}</h4> 
         <h4 class="Frm">${latestBooking.departureDate}</h4> 
         <h4 class="Reto">${latestBooking.arrivalDate}</h4> 
-        <button class="Bookb">Book</button>`;
-        
+        <button onclick="Book()" class="Bookb">Book</button>`;
+
         document.getElementById('no4').innerHTML =
-         `<h4 class="Dep">${latestBooking.departureCity}</h4> 
+        `<h4 class="Dep">${latestBooking.departureCity}</h4> 
+        <span class="aro"><i class="fa-solid fa-arrows-left-right"></i></span>
         <h4 class="Ret">${latestBooking.arrivalCity}</h4> 
         <h4 class="Frm">${latestBooking.departureDate}</h4> 
         <h4 class="Reto">${latestBooking.arrivalDate}</h4> 
-        <button class="Bookb">Book</button>`;
+        <button onclick="Book()" class="Bookb">Book</button>`;
     }
 }
 
@@ -43,46 +87,48 @@ function UpdateloadBooking() {
 
     if (ticket.length > 0) {
         const UlatestBooking = ticket[ticket.length - 1];
-        document.getElementById("name").value = UlatestBooking.Name || '';
         document.getElementById("dep").value = UlatestBooking.departureCity || '';
         document.getElementById("arr").value = UlatestBooking.arrivalCity || '';
         document.getElementById("depD").value = UlatestBooking.departureDate || '';
         document.getElementById("arrD").value = UlatestBooking.arrivalDate || '';
     
         document.getElementById('no2').innerHTML = 
-        `<h4 class="Dep">${UlatestBooking.departureCity}</h4> 
+        `<h4 class="Dep">${UlatestBooking.departureCity}</h4> <span class = "aro"><i class="fa fa-arrows-h"></i></span> 
         <h4 class="Ret">${UlatestBooking.arrivalCity}</h4> 
         <h4 class="Frm">${UlatestBooking.departureDate}</h4> 
-        <h4 class="Reto">${UlatestBooking.arrivalDate}</h4>`;
+        <h4 class="Reto">${UlatestBooking.arrivalDate}</h4>
+        <button onclick="Book()" class="Bookb">Book</button>`;
         
         document.getElementById('no3').innerHTML = 
-        `<h4 class="Dep">${UlatestBooking.departureCity}</h4> 
+        `<h4 class="Dep">${UlatestBooking.departureCity}</h4> <span class = "aro"><i class="fa fa-arrows-h"></i></span>
         <h4 class="Ret">${UlatestBooking.arrivalCity}</h4> 
         <h4 class="Frm">${UlatestBooking.departureDate}</h4> 
-        <h4 class="Reto">${UlatestBooking.arrivalDate}</h4>`;
+        <h4 class="Reto">${UlatestBooking.arrivalDate}</h4>
+        <button onclick="Book()" class="Bookb">Book</button>`;
         
         document.getElementById('no4').innerHTML = 
-        `<h4 class="Dep">${UlatestBooking.departureCity}</h4> 
+        `<h4 class="Dep">${UlatestBooking.departureCity}</h4> <span class = "aro"><i class="fa fa-arrows-h"></i></span>
         <h4 class="Ret">${UlatestBooking.arrivalCity}</h4> 
         <h4 class="Frm">${UlatestBooking.departureDate}</h4> 
-        <h4 class="Reto">${UlatestBooking.arrivalDate}</h4>`;
+        <h4 class="Reto">${UlatestBooking.arrivalDate}</h4>
+        <button onclick="Book()" class="Bookb">Book</button>`;
     }
 }
 
 function updateBooking() {
-    const Name = document.getElementById('name').value;
     const departureCity = document.getElementById('dep').value;
     const arrivalCity = document.getElementById('arr').value;
     const departureDate = document.getElementById('depD').value;
     const arrivalDate = document.getElementById('arrD').value;
+    const passangers = document.getElementById('passan').value;
 
-    if (Name && departureCity && arrivalCity && departureDate && arrivalDate) {
+    if (departureCity && arrivalCity && departureDate && arrivalDate && passangers) {
         const updatedBookings = {
-            Name,
             departureCity,
             arrivalCity,
             departureDate,
             arrivalDate,
+            passangers
         };
 
         let ticket = JSON.parse(localStorage.getItem('ticket')) || [];
@@ -98,4 +144,5 @@ function updateBooking() {
 
 function Book() {
     updateBooking();
+    window.location.href = "../Admin/ManagementPage.html";
 }
